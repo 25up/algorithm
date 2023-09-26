0bcfbd01d1370559a5740223639d4932b574a479
25up <135006066+25up@users.noreply.github.com>
2023-09-26 15:22:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+public static int solution(int[] numbers){
+        int answer=0;
+
+        for(int i = 0 ;i<numbers.length;i++)
+            answer+=numbers[i];
+
+        answer=45-answer;
+        return answer;
+
+    }
+}
