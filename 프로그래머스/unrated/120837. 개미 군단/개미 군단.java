f41d26e6b06ecd40f559dc7964488a208e2b58d9
25up <135006066+25up@users.noreply.github.com>
2023-10-18 14:31:57 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -1,29 +1,13 @@
 class Solution {
-   public int solution(int hp){
-        int answer=0;
-        int a = 0;
-        int aa= 0;
-        int bb=0;
-        int b= 0;
-        int c=0;
-        
-        if(hp>=5) {
-            a = hp / 5;
-            aa=hp%5;
-            b=aa/3;
-            bb=aa%3;
-            c=bb/1;
-            answer=a+b+c;
-        }
-        else if(hp<5&&hp>=3) {
-            b = hp / 3;
-            bb= hp%3;
-            c= bb/1;
-            answer=b+c;
-        } else if (hp<3) {
-            answer=hp/1;
-        }
+  public int solution(int hp) {
+        int answer = hp / 5;
+        hp %= 5;
 
-        return  answer;
+        answer += hp / 3;
+        hp %= 3;
+
+        answer += hp / 1;
+
+        return answer;
     }
 }
